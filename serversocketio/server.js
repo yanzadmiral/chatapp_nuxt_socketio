38374959bf0c7d39bc33b0466f391ec4b18eadb8
Yayan Cahyani <yayancloud@gmail.com>
2022-02-23 11:57:20 +0700

    oke

---
+++ serversocketio/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("private", (msg) => {
+    console.log(msg);
+    io.emit('chat', msg);
+  });
+});
+
+httpServer.listen(3001);
